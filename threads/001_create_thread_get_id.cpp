d59a1bbc7faabbb6dabbe03c2da49fc5f8ca0c87
David Mc Ken <david.mcken@gmail.com>
2024-10-27 01:06:08 -0400

    Commit current state of threads files, likely incomplete.

--- threads/001_create_thread_get_id.cpp
+++ threads/001_create_thread_get_id.cpp
@@ -17,26 +17,38 @@
  * 
  */
 
+// Standard library includes
+#include <chrono>         // std::chrono::seconds
 #include <iostream>       // std::cout
 #include <thread>         // std::thread, std::thread::id, std::this_thread::get_id
-#include <chrono>         // std::chrono::seconds
 
+
+// Global variables
 std::thread::id main_thread_id = std::this_thread::get_id();
 
-void is_main_thread() {
-    if ( main_thread_id == std::this_thread::get_id() ) {
+void is_main_thread()
+{
+    // Get what is our current thread ID.
+    std::thread::id current_thread_id = std::this_thread::get_id();
+
+    if ( main_thread_id == current_thread_id ) {
         std::cout << "This is the main thread." << std::endl;
     } else {
         std::cout << "This is not the main thread." << std::endl;
     }
 
-    std::cout << "Thread ID: " << std::this_thread::get_id() << std::endl;
+    std::cout << "Thread ID: " << current_thread_id << std::endl;
 }
 
 int main() 
 {
+    // Run the function directly in the main thread
     is_main_thread();
+
+    // Create a thread and run the same function with no parameter
     std::thread th (is_main_thread);
+
+    // Join (Wait until it terminates) onto the thread
     th.join();
 
     return 0;
